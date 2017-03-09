fdb1e155bb94869f903eb9f71f2a75447783e3b4
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2017-03-09 09:02:10 +0100

    stroke width 3

--- Trails/app/screens/Map.js
+++ Trails/app/screens/Map.js
@@ -1,19 +1,14 @@
 import React, {
   Component
 } from 'react';
+
 import {
-  Platform,
-  Linking,
   Dimensions
 } from 'react-native';
 
 import {
-  Screen,
-  Text,
-  Button,
-  View
+  Screen
 } from '@shoutem/ui';
-import { NavigationBar } from '@shoutem/ui/navigation';
 
 import MapView from 'react-native-maps';
 
@@ -23,51 +18,12 @@ export default class Map extends Component {
     title: React.PropTypes.string,
   };
 
-  constructor(props) {
-    super(props);
-
-    this.renderNavigateButton = this.renderNavigateButton.bind(this);
-    this.openMaps = this.openMaps.bind(this);
-  }
-
-  openMaps() {
-    const { markers } = this.props;
-    const geoURL = `geo:${markers[0].latitude},${markers[0].longitude}`;
-
-    Linking.canOpenURL(geoURL).then(supported => {
-      if (supported) {
-        Linking.openURL(geoURL);
-      } else {
-        Linking.openURL(`http://maps.apple.com/?ll=${markers[0].latitude},${markers[0].longitude}`);
-      }
-    });
-  }
-
-  renderNavigateButton() {
-    if (Platform.OS === 'ios') {
-      return (
-        <View virtual styleName="container">
-          <Button styleName="clear" onPress={this.openMaps}>
-            <Text>Open in Maps</Text>
-          </Button>
-        </View>
-      );
-    }
-    return null;
-  }
-
   render() {
     const { markers, title } = this.props;
 	const { width, height } = Dimensions.get('window');
 
     return (
       <Screen styleName="full-screen">
-        <NavigationBar
-          styleName="no-border"
-          title={title.toUpperCase()}
-          renderRightComponent={this.renderNavigateButton}
-        />
-		
         <MapView
 			initialRegion={{
 				latitude: markers[0].latitude,
@@ -84,7 +40,7 @@ export default class Map extends Component {
 				coordinates={markers}
 				geodesic
 				strokeColor="#f00"
-				strokeWidth={4}
+				strokeWidth={3}
 			/>
         </MapView>
       </Screen>
